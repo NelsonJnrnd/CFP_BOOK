438a142f09f4f6a7b992aac6a2b340b7460ac798
NelsonJnrnd <nelson.jnrnd@eduge.ch>
2018-02-28 16:21:59 +0100

    envoie des messages

--- CFP_BOOK/envoieMessage.php
+++ CFP_BOOK/envoieMessage.php
@@ -1,3 +1,32 @@
 <?php
+session_start();
+            // Connexion à la base de données
+                try
+                {
+                        $bdd = new PDO('mysql:host=localhost;dbname=cfpbook;charset=utf8', 'root', '');
+                }
+                catch(Exception $e)
+                {
+                        die('Erreur : '.$e->getMessage());
+                }
 
-?>
+
+                $Message = filter_input( INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+                $HeureEnvoi = $_SESSION['HeureEnvoi'];
+                
+if ($Message === NULL) {
+        echo 'veuillez passer par le formulaire !!!';
+}
+ else {
+    
+ 
+        $req = $bdd->prepare('INSERT INTO messages(Message, HeureEnvoi) VALUES(:Message, :HeureEnvoi)');
+        $req->execute(array(
+                'Message' => $Message,
+              'HeureEnvoi' => $HeureEnvoi
+                ));
+
+        echo 'vous etes enregistre';     
+}
+
+
--- CFP_BOOK/global.php
+++ CFP_BOOK/global.php
@@ -1,25 +1,33 @@
+<?php
+session_start();
+
+$_SESSION['HeureEnvoi'] = '';
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
         <title>CFPBook global</title>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link href="css/styleAll.css" rel="stylesheet" type="text/css"/>
-  </head>
+    </head>
 
     <body>
-      <header><?php include 'navbar.php'; ?></header>
-      <div class="card-texte">
-        <h4>global</h4></br>
-        <form action="envoieMessage.php" method="post">
-            <input class="w3-input" type="text" name="message">
-        </form>
-      </div>
-      <footer><?php include 'footer.php'; ?></footer>
+        <header><?php include 'navbar.php'; ?></header>
+        <div class="card-texte">
+            <h4>global</h4></br>
+            <form action="global.php" method="post" onsubmit="envoiMessage">
+                <input class="w3-input" type="text" name="message">
+            </form>
+        </div>
+        <footer><?php include 'footer.php'; ?></footer>
+        <script type="text/javascript">
+            function envoiMessage() {
+<?php
+$_SESSION['HeureEnvoi'] = date("Y-m-d H:i:s");
+include_once 'envoieMessage.php';
+?>
+            }
+        </script>
     </body>
 </html>
