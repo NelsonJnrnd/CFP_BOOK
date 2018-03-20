d357f97e0b06715725852ed22f1ea475dbe53a43
Nyagirl <amandine.pl@eduge.ch>
2018-03-20 16:01:13 +0100

    BDD Compte admin et structure propre

--- cfpbook.sql
+++ cfpbook.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 13 Mars 2018 à 16:10
+-- Généré le :  Mar 20 Mars 2018 à 15:58
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.4.24
 
@@ -33,44 +33,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `idUtilisateur` int(11) NOT NULL,
   PRIMARY KEY (`idMessage`),
   KEY `messages_ibfk_1` (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=51 ;
-
---
--- Contenu de la table `messages`
---
-
-INSERT INTO `messages` (`idMessage`, `Message`, `HeureEnvoi`, `idUtilisateur`) VALUES
-(10, 'sasa', '2018-03-13 14:57:37', 1),
-(11, 'x', '2018-03-13 15:05:57', 2),
-(22, 'vcx', '2018-03-13 15:57:25', 2),
-(23, 'vcx', '2018-03-13 15:57:26', 2),
-(24, 'vcx', '2018-03-13 15:57:27', 2),
-(25, 'vcx', '2018-03-13 15:57:28', 2),
-(26, 'vcx', '2018-03-13 15:57:29', 2),
-(27, 'vcx', '2018-03-13 15:57:30', 2),
-(28, 'vcx', '2018-03-13 15:57:32', 2),
-(29, 'gfjhfg', '2018-03-13 15:57:37', 2),
-(30, 'gfjhfg', '2018-03-13 15:59:17', 2),
-(31, 'gfjhfg', '2018-03-13 15:59:18', 2),
-(32, 'gfjhfggds', '2018-03-13 15:59:20', 2),
-(33, 'dfg', '2018-03-13 15:59:43', 2),
-(34, 'dfg', '2018-03-13 15:59:45', 2),
-(35, 'dsfdsfed', '2018-03-13 16:00:02', 2),
-(36, 'dfsd', '2018-03-13 16:00:03', 2),
-(37, 'sdf', '2018-03-13 16:00:04', 2),
-(38, 'dfsdds', '2018-03-13 16:00:05', 2),
-(39, 'dsfsd', '2018-03-13 16:00:07', 2),
-(40, 'sdfsd', '2018-03-13 16:00:09', 2),
-(41, 'sdfsd', '2018-03-13 16:00:11', 2),
-(42, 'dsfs', '2018-03-13 16:00:12', 2),
-(43, 'sdf', '2018-03-13 16:00:13', 2),
-(44, 'dsfsdf', '2018-03-13 16:00:14', 2),
-(45, 'sdf', '2018-03-13 16:00:15', 2),
-(46, 'x', '2018-03-13 16:04:48', 2),
-(47, 'ss', '2018-03-13 16:04:51', 2),
-(48, 'fsdf', '2018-03-13 16:09:41', 2),
-(49, 'dsf', '2018-03-13 16:09:43', 2),
-(50, 'dsf', '2018-03-13 16:09:44', 2);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=69 ;
 
 -- --------------------------------------------------------
 
@@ -88,15 +51,14 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `pdp` longblob,
   `email` varchar(1000) NOT NULL,
   PRIMARY KEY (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='informations Utilisateurs' AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='informations Utilisateurs' AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `age`, `branche`, `psw`, `pdp`, `email`) VALUES
-(1, 'Mieville', 'Vanille', 19, 'Informatique', 'VanilleM', NULL, 'vanille.pouly@gmail.com'),
-(2, 'Martins', 'Dani', 18, 'Informatique', 'Tarte', NULL, 'daniat@hotmail.fr');
+(5, 'admin', 'admin', 42, 'Informatique', 'AdminSuperCfp_book', NULL, 'admin@eduge.ch');
 
 --
 -- Contraintes pour les tables exportées
