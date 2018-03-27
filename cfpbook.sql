65ebcb9de687b6261673ed63347e14ee549652f5
Nyagirl <amandine.pl@eduge.ch>
2018-03-27 14:13:40 +0200

    ajout dans a propos informations

--- cfpbook.sql
+++ cfpbook.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 20 Mars 2018 à 15:58
+-- Généré le :  Mar 27 Mars 2018 à 14:12
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.4.24
 
@@ -33,7 +33,14 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `idUtilisateur` int(11) NOT NULL,
   PRIMARY KEY (`idMessage`),
   KEY `messages_ibfk_1` (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=69 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `messages`
+--
+
+INSERT INTO `messages` (`idMessage`, `Message`, `HeureEnvoi`, `idUtilisateur`) VALUES
+(1, 'salut', '2018-03-27 14:05:52', 6);
 
 -- --------------------------------------------------------
 
@@ -45,20 +52,23 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(30) NOT NULL,
-  `age` int(11) NOT NULL,
+  `dateNaissance` date NOT NULL,
   `branche` varchar(100) NOT NULL,
   `psw` varchar(10000) NOT NULL,
   `pdp` longblob,
   `email` varchar(1000) NOT NULL,
   PRIMARY KEY (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='informations Utilisateurs' AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='informations Utilisateurs' AUTO_INCREMENT=11 ;
 
 --
 -- Contenu de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `age`, `branche`, `psw`, `pdp`, `email`) VALUES
-(5, 'admin', 'admin', 42, 'Informatique', 'AdminSuperCfp_book', NULL, 'admin@eduge.ch');
+INSERT INTO `utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `dateNaissance`, `branche`, `psw`, `pdp`, `email`) VALUES
+(5, 'admin', 'admin', '0000-00-00', 'Informatique', 'AdminSuperCfp_book', NULL, 'admin@eduge.ch'),
+(6, 'admin', 'admin', '2018-03-09', '', 'root', NULL, 'root@root.root'),
+(9, 'pouly', 'amandine', '0000-00-00', 'Informatique', '1234', NULL, 'amandine.pl@eduge.ch'),
+(10, 'autre', 'personne', '0000-00-00', 'Polymecanique', '1234', NULL, 'compte.demo@eduge.ch');
 
 --
 -- Contraintes pour les tables exportées
